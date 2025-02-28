9379eecb15d7e2aff4eb587cf7fe1753a6e6fb13
qasimsalam <qasimsalam@gmail.com>
2025-02-28 02:55:19 -0800

    Add public-samples/marketplace-8l5lv5/src/ios/ios/AITalentMarketplace/main.m

---
+++ src/ios/ios/AITalentMarketplace/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AITalentMarketplace
+//
+//  Created for AI Talent Marketplace
+//
+
+#import <UIKit/UIKit.h>        // iOS SDK
+#import "AppDelegate.h"        // Class responsible for managing the application lifecycle
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
